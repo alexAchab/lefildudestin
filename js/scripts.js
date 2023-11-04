de0bac0a96c3fcba9458eca94fba0ac223bce9e8
alexAchab <134447204+alexAchab@users.noreply.github.com>
2023-11-04 13:52:16 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
+//Count results
 let count = 1;
+
+//Pin scrolling to bottom with news results
+const scrollToBottom = () => {
+    const resultsDiv = document.getElementById('results');
+    const anchorDiv = document.getElementById('anchor');
+    resultsDiv.scrollTop = anchorDiv.offsetTop;
+}
+
+//Roll thes dices!
 const rollDice = () => {
     const getIntegerValue = (elementId, defaultValue = 0) => {
         const value = parseInt(document.getElementById(elementId).value);
@@ -19,16 +29,20 @@ const rollDice = () => {
     const anchor = document.getElementById('anchor');
     const results = document.getElementById('results');
 
+    //Results in HTML paragraphs
     let resultParagraph = document.createElement('p');
     resultParagraph.classList.add("result");
     results.insertBefore(resultParagraph, anchor);
     resultParagraph.textContent = `#${count} - Le destin a jeté les dés : `;
 
+    //Numbers within a span for CSS
     let resultSpan = document.createElement('span');
     resultParagraph.appendChild(resultSpan);
     resultSpan.textContent = result;
 
     count++;
+
+    scrollToBottom();
 };
 
 document.getElementById('roll').addEventListener('click', rollDice);
